60ae86afdce49820124bc2ca0310672f15f7d653
GreenCatInForest <event2016.2017@gmail.com>
2023-11-08 18:06:04 +0000

    add basic banner style bootstrap

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,12 @@
 export const Banner = ({ title, subTitle }) => {
   return (
-    <div>
-      <div>{title}</div>
-      <div>{subTitle}</div>
+    <div className="container w-75 container col-sm-auto text-center border border-1 border-black p-5">
+      <div className="row">
+        <div className="col">
+          <h1>{title}</h1>
+          <h2 className="fs-4">{subTitle}</h2>
+        </div>
+      </div>
     </div>
   );
 };
