fc8677168711574ffab8cfc57de3feeb34d8323e
GreenCatInForest <event2016.2017@gmail.com>
2023-11-08 18:23:38 +0000

    style basic WordCounter.js bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { WordCounter } from "./components/WordCounter";
 
 export const App = () => {
   return (
-    <div>
+    <div className="container text-center border border-1 border-black p-5">
       <Banner title="Word Counter" subTitle="Paste or type your text here" />
       <WordCounter />
     </div>
--- src/components/WordCounter.js
+++ src/components/WordCounter.js
@@ -1,3 +1,15 @@
 export const WordCounter = () => {
-  return <div>WordCounter</div>;
+  return (
+    <div>
+      <div className="mt-3 container w-75 container col-sm-auto text-center border border-1 border-black p-3 p-sm-5">
+        <textarea
+          class="form-control"
+          id="exampleFormControlTextarea1"
+          rows="8"
+          placeholder="Add your words here..."
+        ></textarea>
+      </div>
+      <div className="mt-3 fw-bolder">Word Count: 0</div>
+    </div>
+  );
 };
