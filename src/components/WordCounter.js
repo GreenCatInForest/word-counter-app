c99ec20e804f9774b298f4846082941ac18a618d
GreenCatInForest <event2016.2017@gmail.com>
2023-11-08 18:31:14 +0000

    set initial value for state wordCounter

--- src/components/WordCounter.js
+++ src/components/WordCounter.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export const WordCounter = () => {
+  const [wordCounter, setWordCounter] = useState(0);
+
   return (
     <div>
       <div className="mt-3 container w-75 container col-sm-auto text-center border border-1 border-black p-3 p-sm-5">
@@ -9,7 +13,7 @@ export const WordCounter = () => {
           placeholder="Add your words here..."
         ></textarea>
       </div>
-      <div className="mt-3 fw-bolder">Word Count: 0</div>
+      <div className="mt-3 fw-bolder">Word Count: {wordCounter}</div>
     </div>
   );
 };
