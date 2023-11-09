abbb24635e8c774fa197e76cb27a348b96542bf1
GreenCatInForest <event2016.2017@gmail.com>
2023-11-09 01:22:22 +0000

    add button

--- src/components/WordCounter.js
+++ src/components/WordCounter.js
@@ -7,13 +7,17 @@ export const WordCounter = () => {
     const counterValue = event.target.value;
     const cleanCounterValue = counterValue.replace(/ +/g, " ");
     const wordCounterValue = cleanCounterValue.split(" ").length;
-
     setWordCounter(wordCounterValue);
   };
 
+  const handleReset = () => {
+    // document.querySelector("#wordCounterTextArea").reset();
+    setWordCounter(0);
+  };
+
   return (
-    <div>
-      <div className="mt-3 container w-75 container col-sm-auto text-center border border-1 border-black p-3 p-sm-5">
+    <div className="mt-3 container w-75 container col-sm-auto text-center border border-1 border-black p-3 p-sm-5 bg-light bg-opacity-25">
+      <div className="">
         <textarea
           className="form-control"
           id="wordCounterTextArea"
@@ -22,7 +26,22 @@ export const WordCounter = () => {
           onChange={handleChange}
         ></textarea>
       </div>
-      <div className="mt-3 fw-bolder">Word Count: {wordCounter}</div>
+      <div
+        className="mt-3 fw-bolder bg-info bg-gradient rounded-2"
+        style={{
+          backgroundColor: "info",
+          fontFamily: "Ubuntu",
+        }}
+      >
+        Word Count: {wordCounter}
+      </div>
+      <button
+        type="button"
+        onClick={handleReset}
+        className="mt-3 fw-bolder container col-sm-auto bg-secondary bg-gradient rounded-2"
+      >
+        Reset
+      </button>
     </div>
   );
 };
