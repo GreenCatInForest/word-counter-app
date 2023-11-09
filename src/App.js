73bd85223a65fe5a6664cb94261567b91b28e356
GreenCatInForest <event2016.2017@gmail.com>
2023-11-09 00:42:32 +0000

    styled banner add backgrounds

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { Banner } from "./components/Banner";
 import { WordCounter } from "./components/WordCounter";
+
 // import { PracticeComponent } from "./components/PracticeComponent";
 
 export const App = () => {
   return (
-    <div className="container text-center border border-1 border-black p-5">
-      <Banner title="Word Counter" subTitle="Paste or type your text here" />
+    <div
+      className="container text-center border border-1 border-black p-5 img-fluid"
+      style={{
+        backgroundImage:
+          'url("https://images.unsplash.com/photo-1685480274296-1bbccf9e2396?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwcm9maWxlLXBhZ2V8OTl8fHxlbnwwfHx8fHw%3D")',
+        backgroundSize: "100%",
+        backgroundPosition: "center",
+        backgroundColor: "rgba(255, 255, 128, .5)",
+      }}
+    >
+      <Banner title="Word Counter" subTitle="Paste or type your text here:" />
       <WordCounter />
-      {/* <PracticeComponent /> */}
     </div>
   );
 };
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,23 @@
 export const Banner = ({ title, subTitle }) => {
   return (
-    <div className="container w-75 container col-sm-auto text-center border border-1 border-black p-5">
+    <div className="container w-75 container col-sm-auto text-center border border-1 border-black p-4 bg-info bg-gradient bg-opacity-50 rounded-2">
       <div className="row">
         <div className="col">
-          <h1>{title}</h1>
-          <h2 className="fs-4">{subTitle}</h2>
+          <h1
+            style={{
+              fontFamily: "Ubuntu",
+            }}
+          >
+            {title}
+          </h1>
+          <h2
+            className="fs-4"
+            style={{
+              fontFamily: "Ubuntu",
+            }}
+          >
+            {subTitle}
+          </h2>
         </div>
       </div>
     </div>
